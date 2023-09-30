9faa7a15f23777f5cb6405c823a955ea8b1aedd0
Школьный Алексей <akademegex36512@gmail.com>
2023-09-30 10:59:53 +0300

    Добавлен файл time.pas

---
+++ time.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var Year:=ReadInteger('Введите номер года:');
+  if (Year mod 4=0) and ((Year mod 100<>0) or (Year mod 400<>0)) then Println('Год високосный')
+  else Println('Год не високосный');
+end.
