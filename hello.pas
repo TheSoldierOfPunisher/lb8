8715107e8cdd0f90e15a4a6a3919409f315d0be7
Школьный Алексей <akademegex36512@gmail.com>
2023-09-30 10:48:17 +0300

    Введено опредение времени суток

--- hello.pas
+++ hello.pas
@@ -1,1 +1,8 @@
-﻿Println('Привет, мир!');
+﻿begin
+  var Hour:=ReadInteger('Введите текущий час от 0 до 23:');
+  if Hour in 4..10 then Println('Доброе утро, мир!')
+  else if Hour in 11..16 then Println('Добрый день, мир!')
+  else if Hour in 17..22 then Println('Добрый вечер, мир!')
+  else Println('Доброй ночи, мир!');
+end.
+
