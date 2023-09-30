e3a4205ba8f6127fdfe5592fa3eb4b35c186b5cc
Школьный Алексей <akademegex36512@gmail.com>
2023-09-30 11:02:15 +0300

    Добавлен вывод количества дней в году

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿begin
   var Year:=ReadInteger('Введите номер года:');
-  if (Year mod 4=0) and ((Year mod 100<>0) or (Year mod 400<>0)) then Println('Год високосный')
-  else Println('Год не високосный');
+  if (Year mod 4=0) and ((Year mod 100<>0) or (Year mod 400<>0)) then Println('Год високосный. 366 дней в году.')
+  else Println('Год не високосный. 365 дней в году.');
 end.
